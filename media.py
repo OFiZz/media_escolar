ce4debb2e1f7c0ce5340379385be2fc21ccb0793
mininumalkinho <luck15santana@gmail.com>
2022-01-17 17:42:38 -0300

    Update versao-1

--- media.py
+++ media.py
@@ -1,24 +1,44 @@
+def linha():
+    print('-' * 15)
+
+
 boletim = {}
-calculadora = []
+todas_notas = []
 soma = 0
 
+nome = input('Digite o nome do aluno:')
+boletim['Aluno'] = nome
+aprovacao = int(input('Qual a media para aprovação da instituição? '))
 
 while True:
-    materia = input('digite o nome da materia ou 0 para exibir a media: ')
-    if materia == '0':
+    materia = input('digite o nome da materia ou fim para exibir a media: ')
+    materia.lower
+    if materia == 'fim':
         break
-    nota = int(input('digite uma nota para {}: '.format(materia)))
-    calculadora.append(nota)
+    linha()
+    nota = float(input('digite uma nota para {}: '.format(materia)))
+    linha()
+    todas_notas.append(nota)
     boletim[materia] = nota
     
-for i in calculadora:
+for i in todas_notas:
     soma += i
 
-media = soma / len(calculadora)
+media = soma / len(todas_notas)
 boletim['media'] = media
 
+if media < aprovacao:
+    print('Sua media não atende os requisitos minimos de aprovação')
+    linha()
+    print('Situação: Reprovado')
+else:
+    print('Muito bem! você superou os requisitos minimos de aprovação')
+    linha()
+    print('Situação: Aprovado') 
 
 print(boletim)
+
+
 
 
 
