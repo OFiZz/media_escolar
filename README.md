c1592a11d808ac26e285b74f2cb48ef4eeded3d0
mininumalkinho <luck15santana@gmail.com>
2022-01-19 21:50:19 -0300

    Vesão-2

---
+++ README.md
@@ -0,0 +1,34 @@
+###  										Média escolar :school:
+
+funcionalidades: Calcular media escolar
+
+Sobre: 
+  Estive durante um tempo com o sentimento de necessidade de criar um programa no qual eu pudesse exercer todo meu conhecimento de programação, a partir desse ponto iniciei a criação desse meu primeiro projeto em python. Entendo que ele pode estar simples, no entanto pretendo atualiza-lo e corrigi-lo enquanto adquiro e aperfeiçoou minhas habilidades com python.
+
+Objetivos:
+
+* Armazenar o nome e a nota de cada matéria em um dicionário python;
+* Calcular a média final com base nas notas fornecidas pelo usuário;
+* Fornecer uma espécie de de boletim escolar com as notas e nomes de materias em forma de dicionário;
+* Realizar um calculo para mostrar ao usuario a nota na qual ele precisará conquistar nos próximos períodos avaliativos.
+
+
+
+Recomendações de uso:
+
+* Esse programa foi criado para calcular media escolar, ciente disso, aconselho ao usuário utilizar nome de materias e notas populares para uma imersão de melhor qualidade.                                         exemplo: 
+
+  ​	português : nota 8 (materia do ensino médio brasileiro e nota positiva com base de 0 a 10);
+
+* Não utilizei tecnicas intermediárias de formatação de string, ou seja, evite o uso de acentos de qualquer tipo e/ou "ç" pois pode gerar possiveis erros de execução.
+
+
+
+Fontes de inspiração:
+
+* Livro introdução a programação com python algoritimos e logica para iniciantes. Escrito por Nilo Ney Coutinho Menezes.
+
+  
+
+
+
--- media.py
+++ media.py
@@ -1,19 +1,51 @@
 def linha():
     print('-' * 15)
 
+def calculo(nota, contagem, media_minima):
+    pont_max = media_minima * contagem
+    resto = pont_max - nota
+    restante = contagem - 1
+    if restante == 0:
+        restante +=1
+        fim = resto / restante   
+        return fim     
+    else:
+        fim = resto / restante
+        return fim
 
 boletim = {}
 todas_notas = []
 soma = 0
 
-nome = input('Digite o nome do aluno:')
+
+nome = input('Digite o nome do aluno: ')
 boletim['Aluno'] = nome
+linha()
 aprovacao = int(input('Qual a media para aprovação da instituição? '))
+linha()
+
+while True:
+    periodo = input('Sua escola divide as notas anuais em bimestral, trimestral ou semestral?  ')
+    periodo.lower()
+    linha()
+    if periodo == 'bimestral':
+        duracao = 4   
+        break  
+    elif periodo == 'trimestral':
+        duracao = 3
+        break
+    elif periodo == 'semestral':
+        duracao = 1
+        break     
+    else:
+        print('Digite apenas uma das palavras chaves forneciadas a seguir: bimestral, trimestral ou semestral')
+        linha()
 
 while True:
     materia = input('digite o nome da materia ou fim para exibir a media: ')
     materia.lower
     if materia == 'fim':
+        linha()
         break
     linha()
     nota = float(input('digite uma nota para {}: '.format(materia)))
@@ -26,20 +58,52 @@ for i in todas_notas:
 
 media = soma / len(todas_notas)
 boletim['media'] = media
+desejo = calculo(media, duracao, aprovacao)
 
 if media < aprovacao:
     print('Sua media não atende os requisitos minimos de aprovação')
     linha()
-    print('Situação: Reprovado')
+    if periodo == 'bimestral':
+        print('Situação: Bimestre reprovado')
+        linha()
+    elif periodo == 'trimestral':
+        print('Situação: Trimestre reprovado')
+        linha()
+    else:
+        print('Situação: Semestre reprovado')
+        print('Você foi reprovado nesse semestre, pois faltaram: {:.2f} pontos para atingir a media '.format(desejo))
+        linha()
 else:
     print('Muito bem! você superou os requisitos minimos de aprovação')
     linha()
-    print('Situação: Aprovado') 
+    if periodo == 'bimestral':
+        print('Situação: Bimestre Aprovado') 
+        linha()
+    elif periodo == 'trimestral':
+        print('Situação: Trimestre aprovado')
+        linha()
+    else:
+        print('Situação: Semestre aprovado')
+        if desejo < 1:    
+            print('Mantenha-se na media {} nos proximos semestres para ser constantemente aprovado!'.format(aprovacao))
+            linha()
 
-print(boletim)
+for e in boletim:
+    print(f'{e}: {boletim[e]}')
 
+linha()
+print('Média: {:.2f}'.format(boletim['media']))
+linha()
 
-
+if periodo == 'bimestral':
+    print('Você precisará manter a seguinte nota nos proximos bimestres para ser aprovado no ano letivo: {:.2f}'.format(desejo))
+    print('Terminando nossos serviços...')
+elif periodo == 'trimestral':
+    print('Você precisará manter a seguinte nota nos próximos trimestre para ser aprovado no ano letivo atual: {:.2f}'.format(desejo))
+    print('Teminando nossos serviços...')
+else:
+    print('Terminando nossos serviços...')
+    
 
 
 
