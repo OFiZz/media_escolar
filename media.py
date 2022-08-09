f0f6ff91d9c53296cf0f74d1a2061c1d9ae2a592
Diego Silva <101846146+Dhyigo@users.noreply.github.com>
2022-08-09 03:10:34 -0300

    Update media.py
    tentei não mexer muito no código original.

--- media.py
+++ media.py
@@ -1,58 +1,51 @@
 def linha():
     print('-' * 15)
 
+
 def calculo(nota, contagem, media_minima):
     pont_max = media_minima * contagem
     resto = pont_max - nota
     restante = contagem - 1
     if restante == 0:
-        restante +=1
-        fim = resto / restante   
-        return fim     
+        restante += 1
+        fim = resto / restante
+        return fim
     else:
         fim = resto / restante
         return fim
 
-boletim = {}
+
+boletim = {'Aluno': input('Digite o nome do aluno: ').capitalize()}
+sistema_media = {'bimestral': 4, 'trimestral': 3, 'semestral': 1}
 todas_notas = []
 soma = 0
-
-
-nome = input('Digite o nome do aluno: ')
-boletim['Aluno'] = nome
 linha()
 aprovacao = int(input('Qual a media para aprovação da instituição? '))
 linha()
 
 while True:
-    periodo = input('Sua escola divide as notas anuais em bimestral, trimestral ou semestral?  ')
-    periodo.lower()
-    linha()
-    if periodo == 'bimestral':
-        duracao = 4   
-        break  
-    elif periodo == 'trimestral':
-        duracao = 3
-        break
-    elif periodo == 'semestral':
-        duracao = 1
-        break     
-    else:
+    try:
+        periodo = input('Sua escola divide as notas anuais em bimestral, trimestral ou semestral?  ').lower()
+        duracao = sistema_media[periodo]
+    except KeyError:
         print('Digite apenas uma das palavras chaves forneciadas a seguir: bimestral, trimestral ou semestral')
         linha()
 
+    else:
+        break
+
 while True:
-    materia = input('digite o nome da materia ou fim para exibir a media: ')
-    materia.lower
+    materia = input('digite o nome da materia ou fim para exibir a media: ').lower()
+
     if materia == 'fim':
         linha()
         break
     linha()
-    nota = float(input('digite uma nota para {}: '.format(materia)))
+    nota = float(input('digite uma nota para {}: '.format(materia.capitalize())))
     linha()
     todas_notas.append(nota)
     boletim[materia] = nota
-    
+
 for i in todas_notas:
     soma += i
 
@@ -63,52 +56,24 @@ desejo = calculo(media, duracao, aprovacao)
 if media < aprovacao:
     print('Sua media não atende os requisitos minimos de aprovação')
     linha()
-    if periodo == 'bimestral':
-        print('Situação: Bimestre reprovado')
-        linha()
-    elif periodo == 'trimestral':
-        print('Situação: Trimestre reprovado')
-        linha()
-    else:
-        print('Situação: Semestre reprovado')
-        print('Você foi reprovado nesse semestre, pois faltaram: {:.2f} pontos para atingir a media '.format(desejo))
-        linha()
+    print(f'Situação: {periodo.capitalize()} reprovado')
+    linha()
+
 else:
     print('Muito bem! você superou os requisitos minimos de aprovação')
     linha()
-    if periodo == 'bimestral':
-        print('Situação: Bimestre Aprovado') 
-        linha()
-    elif periodo == 'trimestral':
-        print('Situação: Trimestre aprovado')
-        linha()
-    else:
-        print('Situação: Semestre aprovado')
-        if desejo < 1:    
-            print('Mantenha-se na media {} nos proximos semestres para ser constantemente aprovado!'.format(aprovacao))
-            linha()
+
+    print(f'Situação: {periodo} Aprovado')
+    linha()
 
 for e in boletim:
+    from time import sleep
+    sleep(1)
     print(f'{e}: {boletim[e]}')
 
 linha()
 print('Média: {:.2f}'.format(boletim['media']))
 linha()
 
-if periodo == 'bimestral':
-    print('Você precisará manter a seguinte nota nos proximos bimestres para ser aprovado no ano letivo: {:.2f}'.format(desejo))
-    print('Terminando nossos serviços...')
-elif periodo == 'trimestral':
-    print('Você precisará manter a seguinte nota nos próximos trimestre para ser aprovado no ano letivo atual: {:.2f}'.format(desejo))
-    print('Teminando nossos serviços...')
-else:
-    print('Terminando nossos serviços...')
-    
-
-
-
-
-
-
-
-
+print(f'Você precisará manter a seguinte nota nos proximos {periodo} para ser aprovado no ano letivo: {desejo:.2f}')
+print('Terminando nossos serviços...')
